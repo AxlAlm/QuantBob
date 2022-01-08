8418daacb73d2c782c06ab83bac648ddaa8fa308
Axel Almquist <axel@almquist.io>
2022-01-08 17:46:34 +0100

    found a way to read parts of the parquet files. We can also first read id and eras column and then create the split groups first so we know how much to read for each split for the parquet files

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from quantbob.numerai_dataset import NumerAIDataset
+# from quantbob.numerai_dataset import NumerAIDataset
 
-dataset = NumerAIDataset()
-cv = dataset.create_cvs(n_folds = 10)
+# dataset = NumerAIDataset()
+# cv = dataset.create_cvs(n_folds = 10)
+
+import pyarrow.parquet import ParquetFile
+
+
+F = ParquetFile("/tmp/numerai_data/numerai_validation_data.parquet")
